62ff5ce023372b404e49ac51e3c31bbcbbd7a896
sg0808 <saksham.goel0808@gmail.com>
2023-02-25 23:12:40 +0530

    Create a README.md

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const helmet = require("helmet");
 
 const MongoStore = require('connect-mongo');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = "mongodb://localhost:27017/yelp-camp";
 
 const User = require("./models/user");
 const ExpressError = require("./utils/ExpressError");
